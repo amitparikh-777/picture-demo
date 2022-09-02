667de38be7e89f5a2f01873fb0a1a675fcf1a34f
amitparikh-777 <amit.parikh.npm@gmail.com>
2022-09-01 22:15:00 -0400

    2 files modified, 1 added

--- src/app/items/components/item-list/item-list.component.ts
+++ src/app/items/components/item-list/item-list.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+import { Item } from '../../models/item.types';
+import { ItemsService } from '../../services/items.service';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.css']
+  styleUrls: ['./item-list.component.css'],
 })
 export class ItemListComponent implements OnInit {
+  public itemsList: Item[];
+  private itemsMap = new Map<number, Item>();
 
-  constructor() { }
+  constructor(private itemsService: ItemsService) {}
 
   ngOnInit() {
+    this.prepareItems();
   }
 
+  prepareItems() {
+    this.itemsService
+      .getItems()
+      .pipe(take(1))
+      .subscribe((items: Item[]) => {
+        this.itemsList = items;
+        this.storeItems(items);
+      });
+  }
+
+  storeItems(items: Item[]): void {
+    if (!!!this.itemsMap.size) {
+      items.forEach((i: Item) => {
+        this.itemsMap.set(i.id, i);
+      });
+    }
+  }
 }
--- src/app/items/services/items.service.ts
+++ src/app/items/services/items.service.ts
@@ -1,19 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+import { Item } from '../models/item.types';
 
 @Injectable({ providedIn: 'root' })
 export class ItemsService {
-
-  private url = "https://jsonplaceholder.typicode.com/photos";
+  private url = 'https://jsonplaceholder.typicode.com/photos';
   constructor(private readonly http: HttpClient) {}
 
-
-
-  getItems() {
-    return this.http.get(this.url);
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.url).pipe(shareReplay(1));
   }
 
-  getItemById() {
-
-  }
+  getItemById() {}
 }
