bfa07550f393a17808b1f651c3615073b5cecbff
amitparikh-777 <amit.parikh.npm@gmail.com>
2022-09-01 22:51:17 -0400

    3 files modified, 1 added, 1 deleted

--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ItemListComponent } from './components/item-list/item-list.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, MatCardModule],
   declarations: [ItemListComponent, ItemDetailComponent],
   exports: [ItemDetailComponent, ItemListComponent],
 })
